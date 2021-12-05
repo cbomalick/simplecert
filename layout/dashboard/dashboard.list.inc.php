353025f2895ba3234d11daa0aefc40ca5a999c2f
Collin Bomalick <cbomalick@gmail.com>
2021-12-04 23:03:42 -0600

    Update dashboard.list.inc.php

--- layout/dashboard/dashboard.list.inc.php
+++ layout/dashboard/dashboard.list.inc.php
@@ -10,56 +10,56 @@ Echo"<div class=\"alerts\">
 Echo"
 <div class=\"boxwrapper\">";
 
-        // if($loggedInUser->validatePermissions("ServiceView")){
-            Echo"<div class=\"box largebox\">
-            <div class=\"boxheader\">
-                <h2>Site Details</h2>
-            </div>
-                <div class=\"boxcontent\">";
-                
-                $site = new Site();
-                Echo"<pre>";
-                var_dump($site);
-                Echo"</pre>";
+// if($loggedInUser->validatePermissions("ServiceView")){
+    Echo"<div class=\"box largebox\">
+    <div class=\"boxheader\">
+        <h2>Site Details</h2>
+    </div>
+        <div class=\"boxcontent\">";
+        
+        $site = new Site();
+        Echo"<pre>";
+        var_dump($site);
+        Echo"</pre>";
 
-                Echo"</div>
-            </div>";
+        Echo"</div>
+    </div>";
 
-            Echo"<div class=\"box largebox\">
-            <div class=\"boxheader\">
-                <h2>Current User</h2>
-            </div>
-            <div class=\"boxcontent\">";
+    Echo"<div class=\"box largebox\">
+    <div class=\"boxheader\">
+        <h2>Current User</h2>
+    </div>
+    <div class=\"boxcontent\">";
 
-            Echo"<pre>";
-            var_dump($session->loggedInUser);
-            Echo"</pre>";
+    Echo"<pre>";
+    var_dump($session->loggedInUser);
+    Echo"</pre>";
 
-            //$dashboard->printRecentRecords("Service", $loggedInUser->allowedCompanies);
-                Echo"</div>
-            </div>";
+    //$dashboard->printRecentRecords("Service", $loggedInUser->allowedCompanies);
+        Echo"</div>
+    </div>";
 
-            Echo"<div class=\"box largebox\">
-            <div class=\"boxheader\">
-                <h2>Session Details</h2>
-            </div>
-                <div class=\"boxcontent\">";
-                
-                $site = new Site();
-                Echo"<pre>";
-                var_dump($_SESSION);
-                Echo"</pre>";
+    Echo"<div class=\"box largebox\">
+    <div class=\"boxheader\">
+        <h2>Session Details</h2>
+    </div>
+        <div class=\"boxcontent\">";
+        
+        $site = new Site();
+        Echo"<pre>";
+        var_dump($_SESSION);
+        Echo"</pre>";
 
-                Echo"</div>
-            </div>";
+        Echo"</div>
+    </div>";
 
-        // }
+// }
 
-            Echo"</p>
-            </div>
-            </div>";
-            
-            //$audit = new AuditLog("Update", "Test", "Test");
+    Echo"</p>
+    </div>
+    </div>";
+    
+    //$audit = new AuditLog("Update", "Test", "Test");
 
 Echo"</div>";
 
